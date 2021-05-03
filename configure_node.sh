f5eb3bd524d600011427cf22dc55f613f18a31fe
Travis Nasser <nassertravis@gmail.com>
2021-05-03 14:41:38 -0700

    Updated openmpi install path in configure script and added .bashrc

--- configure_node.sh
+++ configure_node.sh
@@ -16,17 +16,23 @@ export PATH=/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin
 # Configure OpenMPI
 wget https://www.open-mpi.org/software/ompi/v1.8/downloads/openmpi-1.8.1.tar.gz 
 tar -xzf openmpi-1.8.1.tar.gz && cd openmpi-1.8.1
-./configure --prefix="/users/mpiuser/.openmpi"
-make && make install
+./configure --prefix="/home/mpiuser/.openmpi"
+make
+make install
+
+mv /local/.bashrc /user/mpiuser/
 
 # Genererate ssh-keys
 ssh-keygen -t rsa -N "" -f /${USER}/.ssh/id_rsa
 cp ~/.ssh/id_rsa.pub ~/.ssh/authorized_keys
+
+EOF
+echo "Exit mpiuser"
+
+reboot now
 
 # Configure RDMA
 apt-get install libibverbs1 ibverbs-utils librdmacm1 rdmacm-utils libcxgb3-1 libmlx4-1 libmthca1 libnes1 libibcommon1 ibverbs-providers
 wget https://launchpad.net/ubuntu/+source/libibcommon/1.1.2-20090314-1ubuntu1/+build/5845211/+files/libibcommon1_1.1.2-20090314-1ubuntu1_arm64.deb
 dpkg -i libibcommon1_1.1.2-20090314-1ubuntu1_arm64.deb
 
-EOF
-echo "Exit mpiuser"
