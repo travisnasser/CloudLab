1c7df71ea88f4c9ee90df99c52f2576c0a90e77b
Travis Nasser <nassertravis@gmail.com>
2021-05-03 16:54:53 -0700

    Changes to configuration script

--- configure_node.sh
+++ configure_node.sh
@@ -22,7 +22,7 @@ sudo -i -u root bash << \eof1
         make
         make install
 
-        mv /local/.bashrc /home/mpiuser/
+        cp /local/bashrc /home/mpiuser/.bashrc
 
         # Genererate ssh-keys
         ssh-keygen -t rsa -N "" -f /${USER}/.ssh/id_rsa
@@ -30,12 +30,27 @@ sudo -i -u root bash << \eof1
 
         echo "Exiting mipuser"
     eof2
+eof1
 
+sudo -i -u root bash << \eof3
     whoami
     # Configure RDMA
-    apt-get install libibverbs1 ibverbs-utils librdmacm1 rdmacm-utils libcxgb3-1 libmlx4-1 libmthca1 libnes1 libibcommon1 ibverbs-providers
+    apt-get install -y libibverbs1 ibverbs-utils librdmacm1 rdmacm-utils ibverbs-providers    
     wget https://launchpad.net/ubuntu/+source/libibcommon/1.1.2-20090314-1ubuntu1/+build/5845211/+files/libibcommon1_1.1.2-20090314-1ubuntu1_arm64.deb
     dpkg -i libibcommon1_1.1.2-20090314-1ubuntu1_arm64.deb
+   
+    modprobe rdma_cm
+    modprobe ib_uverbs
+    modprobe rdma_ucm
+    modprobe ib_ucm
+    modprobe ib_umad
+    modprobe ib_ipoib
+    modprobe mlx4_ib
+    modprobe mlx4_en
+    modprobe iw_cxgb3
+    modprobe iw_cxgb4
+    modprobe iw_nes
+    modprobe iw_c2
 
     reboot now
-eof1
+eof3
